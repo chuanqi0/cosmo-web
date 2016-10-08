bad632c10128ff895d9b264bd10207f4ea85ff4e
He Hai <hehai@vip.126.com>
2016-10-08 10:51:33 +0800

    Web Commit

---
+++ src/WebBundle/Controller/WebViewController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WebBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+
+
+use AppBundle\Controller\BaseController;
+
+class WebViewController extends BaseController
+{
+}

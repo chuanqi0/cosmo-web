42e25271aa3776c65817a8901750eef3124255d0
He Hai <hehai@vip.126.com>
2016-10-29 16:43:03 +0800

    Mobile Commit

--- web/js/app.cbwa.js
+++ web/js/app.cbwa.js
@@ -2,10 +2,8 @@ var app = angular.module('app', ['ngCookies']).config(function($interpolateProvi
     $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 });
 
-app.controller('BaseCBWAController', function($scope, $cookies, UtilService) {
+app.controller('BaseCBWAController', function($scope, $cookies) {
     $scope.user = null;
-
-    $scope.isMobile = UtilService.isMobile();
 
     if ($cookies.getObject('user')) {
         $scope.user = $cookies.getObject('user');

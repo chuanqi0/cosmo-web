d3fe7aa2d60979158c1810a825517d644ec74f33
zlfomg <846214718@qq.com>
2017-01-20 23:27:26 +0800

    Face Commit
    url

--- web/js/mobile/face.js
+++ web/js/mobile/face.js
@@ -1,105 +1,109 @@
-/**
- * Created by ZLF on 2017/1/20.
- */
-app.controller('FaceController', function($scope, photoUuid, DownloadService) {
+app.controller('SwapFaceCtrl', ['$scope', '$state', function ($scope, $state) {
 
-    $scope.photo = null;
-    $scope.favorList = [];
-    $scope.commentList = [];
-
-    $scope.getPhotoDetail = function () {
-        $.ajax({
-            url: domain + '/api/photo/detail/',
-            type: 'POST',
-            dataType: 'json',
-            data: {
-                photoUuid: photoUuid
-            },
-            async: false,
-            success: function (response) {
-                if (response.status == 0) {
-                    $scope.photo = response.data;
-                    if ($scope.photo.urlList.length == 0) {
-                        var one = {
-                            'url': $scope.photo.url,
-                            'urlThumb': $scope.photo.urlThumb,
-                            'width': $scope.photo.width,
-                            'height': $scope.photo.height
-                        };
-                        $scope.photo.urlList.push(one);
-                    }
-                    console.log($scope.photo);
-                } else {
-                    console.log(response.message);
-                }
-            },
-            error: function (xhr, status, err) {
-                console.log(err);
-            }
-        });
+    $scope.gotoStartMake = function () {
+        window.location.href = base + 'face/model';
     };
 
-    $scope.getFavorList = function () {
-        var data = {
-            photoUuid: photoUuid,
-            page: 1,
-            base: 0
+    $scope.model = {
+        woman: true,
+        man: false,
+        couple: false
+    };
+
+    $scope.changeModel = function (type) {
+        if ($scope.model[type] == true) {
+            return;
+        }
+        $scope.model[type] = !$scope.model[type];
+        for (var key in $scope.model) {
+            if (key != type && $scope.model[type] == true) {
+                $scope.model[key] = false;
+            }
+        }
+    };
+
+    $scope.gotoStepFill = function () {
+        var type = '';
+        for (var key in $scope.model) {
+            if ($scope.model[key] == true) {
+                type = key;
+                break;
+            }
+        }
+        window.location.href = base + 'face/step/fill';
+    };
+
+    $scope.gotoStepPhoto = function () {
+        window.location.href = base + 'face/step/photo';
+    };
+
+    $scope.waterMark = {
+        tag1: '新年快乐',  // 110
+        tag2: '哈哈',  // 190
+        tag3: '',  // 280
+        tag4: '',
+        tag5: '',
+        tag6: '',
+        sourceImage: '',
+        croppedImage: ''
+    };
+
+    // 填充文字
+    $scope.gotoChoosePhoto = function () {
+        $state.go('face.step2photo');
+    };
+
+    if ($state.current.url.indexOf('step') != -1) {
+        var img = new Image();
+        img.src = './img/face/img_model_2.png';
+        // 加载完成开始绘制
+        img.onload = function () {
+            //准备canvas环境
+            var canvas = document.getElementById("poster-canvas");
+            canvas.height = (img.height / img.width) * canvas.width;
+            var ctx = canvas.getContext("2d");
+            // 绘制图片
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+            // 绘制水印
+            var ModelType = 0;
+            //ctx.font = "20px microsoft yahei";
+            ctx.fillStyle = "#000000";
+            ctx.fillText($scope.waterMark.tag1, 16, 110);
+            ctx.fillStyle = "#BA882A";
+            if (ModelType == 1) {
+                ctx.fillStyle = "pink";
+            }
+            ctx.fillText($scope.waterMark.tag2, 16, 190);
+            ctx.fillStyle = "#BA882A";
+            if (ModelType == 1) {
+                ctx.fillStyle = "pink";
+            }
+            ctx.fillText($scope.waterMark.tag3, 16, 280);
+            ctx.fillStyle = "#BA882A";
+            if (ModelType == 1) {
+                ctx.fillStyle = "pink";
+            }
+            ctx.fillText($scope.waterMark.tag4, 16, 110);
+            ctx.fillStyle = "rgba(255,255,255,0.5)";
+            ctx.fillText($scope.waterMark.tag5, 16, 180);
+            ctx.fillStyle = "rgba(255,255,255,0.5)";
+            ctx.fillText($scope.waterMark.tag6, 16, 250);
+        }
+    }
+
+    $scope.myImage = '';
+    $scope.myCroppedImage = '';
+    var handleFileSelect = function (evt) {
+        var file = evt.currentTarget.files[0];
+        var reader = new FileReader();
+        reader.onload = function (evt) {
+            $scope.$apply(function ($scope) {
+                $scope.myImage = evt.target.result;
+                //$scope.waterMark.sourceImage = evt.target.result;
+            });
         };
-        $.ajax({
-            url: domain + '/api/photo/favor/list/',
-            type: 'POST',
-            dataType: 'json',
-            data: data,
-            async: false,
-            success: function (response) {
-                if (response.status == 0) {
-                    var width = window.innerWidth;
-                    $scope.favorList = response.dataList.slice(0, 7);
-                } else {
-                    console.log(response.message);
-                }
-            },
-            error: function (xhr, status, err) {
-                console.log(err);
-            }
-        });
+        reader.readAsDataURL(file);
     };
 
-    $scope.getCommentlist = function () {
-        var data = {
-            photoUuid: photoUuid,
-            page: 1,
-            base: 0,
-            order: 1
-        };
-        $.ajax({
-            url: domain + '/api/photo/comment/list/',
-            type: 'POST',
-            data: data,
-            dataType: 'json',
-            success: function (response) {
-                if (response.status == 0) {
-                    $scope.commentList = response.dataList.slice(0, 3);
-                    $scope.$applyAsync();
-                } else {
-                    console.log(response.message);
-                }
-            },
-            error: function (xhr, status, err) {
-                console.log(err);
-            }
-        });
-    };
-
-    $scope.init = function () {
-        $scope.getPhotoDetail();
-        $scope.getFavorList();
-        $scope.getCommentlist();
-        var height = window.innerHeight;
-        $('.m-share-photo').css('min-height', height);
-    };
-
-    $scope.jumpDownload = function () {
-        DownloadService.download();
-    };
-});
+    angular.element(document.querySelector('#fileInput')).on('change', handleFileSelect);
+}]);

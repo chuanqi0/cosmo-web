e9cb40587dcafeae78673a2332e2ecc1f0f22aa0
zlfomg <846214718@qq.com>
2017-01-22 00:34:14 +0800

    Face size

--- web/js/mobile/face.js
+++ web/js/mobile/face.js
@@ -272,7 +272,7 @@ app.controller('SwapFaceCtrl', ['$scope', '$cookieStore', function ($scope, $coo
                 var crops = faceObj.crops.split('#');
                 var crop1 = $("<img src='" + crops[0] + "'/>")[0];
                 var coord1 = crops[1];
-                ctx.drawImage(crop1, 0, 0, crop1.width, crop1.height, parseInt(coord1.split(',')[0]) - 30, parseInt(coord1.split(',')[1]), 30, 30);
+                ctx.drawImage(crop1, 0, 0, crop1.width, crop1.height, parseInt(coord1.split(',')[0]) - 15, parseInt(coord1.split(',')[1]), 30, 30);
                 if (crops[2]) {
                     var crop2 = $("<img src='" + crops[2] + "'/>")[0];
                     var coord2 = crops[3];

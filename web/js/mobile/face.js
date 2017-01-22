f120eedb8d7ed9ba1bde69a2b86643e7f41bd647
zlfomg <846214718@qq.com>
2017-01-22 13:34:53 +0800

    Face Crop

--- web/js/mobile/face.js
+++ web/js/mobile/face.js
@@ -276,11 +276,11 @@ app.controller('SwapFaceCtrl', ['$scope', '$cookieStore', function ($scope, $coo
                     var crops = faceObj.crops.split('#');
                     var crop1 = $("<img src='" + crops[0] + "'/>")[0];
                     var coord1 = crops[1];
-                    ctx.drawImage(crop1, 0, 0, crop1.width, crop1.height, parseInt(coord1.split(',')[0]), parseInt(coord1.split(',')[1]), cropWidth, cropHeight);
+                    ctx.drawImage(crop1, 0, 0, crop1.width, crop1.height, parseInt(coord1.split(',')[0]) - cropWidth / 2, parseInt(coord1.split(',')[1]), cropWidth, cropHeight);
                     if (crops[2]) {
                         var crop2 = $("<img src='" + crops[2] + "'/>")[0];
                         var coord2 = crops[3];
-                        ctx.drawImage(crop2, 0, 0, crop2.width, crop2.height, parseInt(coord2.split(',')[0]), parseInt(coord2.split(',')[1]), cropWidth, cropHeight);
+                        ctx.drawImage(crop2, 0, 0, crop2.width, crop2.height, parseInt(coord2.split(',')[0]) - cropWidth / 2, parseInt(coord2.split(',')[1]), cropWidth, cropHeight);
                     }
                 } catch (e) {
                     console.log(e);

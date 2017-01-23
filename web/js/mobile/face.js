eb7514151cb42925a74d04f11b3d68d946ededce
zlfomg <846214718@qq.com>
2017-01-23 15:41:24 +0800

    Face  ZZZ

--- web/js/mobile/face.js
+++ web/js/mobile/face.js
@@ -261,23 +261,23 @@ app.controller('SwapFaceCtrl', ['$scope', '$cookieStore', function ($scope, $coo
         if (type == 'woman') {
             ctx.fillStyle = "#FF3366";
         }
-        var toLeft = (canvas.width - 20 * (canvas.width / 343) * (tag4.length.length <= 5 ? tag4.length : 5));
+        var toLeft4 = (canvas.width - 20 * (canvas.width / 343) * (tag4.length <= 5 ? tag4.length : 5));
         tag4 = tag4.substr(0, 5);
-        ctx.fillText(tag4 != '-' ? tag4 : '', toLeft, 110 * (canvas.height / 412), 80 * (canvas.width / 343));
+        ctx.fillText(tag4 != '-' ? tag4 : '', toLeft4, 110 * (canvas.height / 412), 80 * (canvas.width / 343));
         // 文字5
         ctx.fillStyle = "#BA882A";
-        var toLeft = (canvas.width - 20 * (canvas.width / 343) * (tag5.length <= 6 ? tag5.length : 6));
+        var toLeft5 = (canvas.width - 20 * (canvas.width / 343) * (tag5.length <= 6 ? tag5.length : 6));
         tag5 = tag5.substr(0, 6);
-        ctx.fillText(tag5 != '-' ? tag5 : '', toLeft, 180 * (canvas.height / 412), 100 * (canvas.width / 343));
+        ctx.fillText(tag5 != '-' ? tag5 : '', toLeft5, 180 * (canvas.height / 412), 100 * (canvas.width / 343));
         // 文字6
         ctx.font = "22px microsoft yahei";
         ctx.fillStyle = "#000000";
         if (type == 'couple') {
             ctx.fillStyle = "#FFFFFF";
         }
-        var toLeft = (canvas.width - 22 * (canvas.width / 343) * (tag6.length <= 7 ? tag6.length : 7));
+        var toLeft6 = (canvas.width - 22 * (canvas.width / 343) * (tag6.length <= 7 ? tag6.length : 7));
         tag6 = tag6.substr(0, 7);
-        ctx.fillText(tag6 != '-' ? tag6 : '', toLeft, 250 * (canvas.height / 412), 140 * (canvas.width / 343));
+        ctx.fillText(tag6 != '-' ? tag6 : '', toLeft6, 250 * (canvas.height / 412), 140 * (canvas.width / 343));
         // 头像
         try {
             var croppedImg = document.getElementById('cropped-img-1');
